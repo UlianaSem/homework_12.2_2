e90c0d2433371746f3916c3cc04f22ab90897d46
Uliana Semyonova <i@ulianasemyonova.ru>
2023-05-19 20:57:53 +0500

    Update TestArrs class Fix test_get method

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,12 @@
+import unittest
 from utils import arrs
 
+class TestArrs(unittest.TestCase):
+    def test_get(self):
+        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+        with self.assertRaises(IndexError):
+            arrs.get([], 0, "test")
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
-
-
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    def test_slice(self):
+        self.assertEquals(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
+        self.assertEquals(arrs.my_slice([1, 2, 3], 1), [2, 3])
